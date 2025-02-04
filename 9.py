d3a871690a8cf73e0f8f6f710cf24af59c547b47
PaulKrets <patronesse2@protonmail.com>
2025-02-05 00:06:39 +0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -5,6 +5,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from keras.datasets import fashion_mnist
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+
 
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
 
@@ -22,3 +29,13 @@ lr = LogisticRegression(max_iter=1000)
 lr.fit(X_train, y_train)
 lr_predictions = lr.predict(X_test)
 print(f'Логистическая регрессия точность: {accuracy_score(y_test, lr_predictions)}')
+
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(X_train, y_train)
+rf_predictions = rf.predict(X_test)
+print(f'Случайный лес точность: {accuracy_score(y_test, rf_predictions)}')
+
+svm = SVC()
+svm.fit(X_train, y_train)
+svm_predictions = svm.predict(X_test)
+print(f'SVM точность: {accuracy_score(y_test, svm_predictions)}')
